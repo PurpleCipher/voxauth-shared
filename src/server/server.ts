44eeb0bee2bc7e061c25da9bedb619bf51be5fcc
Thompson Edolo <907685+verygreenboi@users.noreply.github.com>
2022-06-11 02:08:46 +0100

    Feature/routing (#18)
    * feat: drop fastify support
    
    * feat: implement routing

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,27 +1,42 @@
-import { FastifyInstance } from "fastify";
-import { Application } from "express";
+import express, { Application, NextFunction, Router } from "express";
 
-export type ServerType = Application | FastifyInstance;
+export type ServerType = Application;
 
 type BaseServerConfig = {
   port: number;
   cb?: (...args: unknown[]) => void;
 };
 export type ExpressServerConfig = {};
-export type FastifyServerConfig = {};
-export type ServerConfig = BaseServerConfig &
-  ExpressServerConfig &
-  FastifyServerConfig;
+export type ServerConfig = BaseServerConfig & ExpressServerConfig;
+
+export type Middleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => void | Promise<void>;
+export type Route = {
+  handler: (router: Router, middlewares: Middleware[]) => Router;
+  middlewares: Middleware[];
+};
 
 export interface IServer {
   app: ServerType;
   config: ServerConfig;
+  router: Router;
+
   // setupMiddlewares?: () => void;
-  // setupRoutes(): void;
+  setupRoutes(routes: Route[]): void;
+
   listen(cb: (...args: unknown[]) => void): Promise<void>;
 }
 
-export abstract class BaseServer implements Pick<IServer, "config"> {
+export abstract class BaseServer
+  implements Pick<IServer, "app" | "config" | "router">
+{
+  app: ServerType = express();
+
+  router: Router = express.Router();
+
   config: ServerConfig;
 
   constructor(config: ServerConfig) {
