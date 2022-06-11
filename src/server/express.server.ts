c7d40bbd4d09ab46be72f724491380a18c283859
Thompson Edolo <907685+verygreenboi@users.noreply.github.com>
2022-06-11 01:17:34 +0100

    fix: change server constructor visibility (#16)

--- src/server/express.server.ts
+++ src/server/express.server.ts
@@ -1,16 +1,11 @@
 import { Application } from "express";
-import { BaseServer, IServer, ServerConfig } from "./server";
+import { BaseServer, IServer } from "./server";
 
 export abstract class ExpressServer extends BaseServer implements IServer {
   abstract app: Application;
 
   abstract setupMiddlewares(): void;
-
   abstract setupRoutes(): void;
-
-  protected constructor(config: ServerConfig) {
-    super(config);
-  }
 
   async listen(cb: (...args: unknown[]) => void): Promise<void> {
     this.app.listen(this.config.port, cb);
--- src/server/fastify.server.ts
+++ src/server/fastify.server.ts
@@ -1,14 +1,9 @@
 import { FastifyInstance } from "fastify";
-import { BaseServer, IServer, ServerConfig } from "./server";
+import { BaseServer, IServer } from "./server";
 
 export abstract class FastifyServer extends BaseServer implements IServer {
   abstract app: FastifyInstance;
-
   abstract setupRoutes(): void;
-
-  protected constructor(config: ServerConfig) {
-    super(config);
-  }
 
   async listen(cb: (...args: unknown[]) => void): Promise<void> {
     await this.app.listen({
