d2c8e81359b6ebc1df9ee934f549ced41436210b
Thompson Edolo <verygreenboi@live.com>
2022-06-11 02:16:27 +0100

    feat: use fluent api for server set up

--- src/server/express.server.ts
+++ src/server/express.server.ts
@@ -4,14 +4,22 @@ import { BaseServer, IServer, Route } from "./server";
 export class ExpressServer extends BaseServer implements IServer {
   app: Application = express();
 
+  routes: Route[] = [];
+
   // setupMiddlewares(): void {}
   // setupRoutes(): void {}
 
   async listen(cb: (...args: unknown[]) => void): Promise<void> {
+    this.setupRoutes(this.routes);
     this.app.listen(this.config.port, cb);
   }
 
-  setupRoutes(routes: Route[]): void {
+  setRoutes(routes: Route[]): IServer {
+    this.routes = routes;
+    return this;
+  }
+
+  private setupRoutes(routes: Route[]): void {
     routes.forEach((route) =>
       this.app.use(route.handler(this.app, route.middlewares))
     );
