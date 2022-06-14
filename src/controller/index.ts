0dfca357e466611fb7d07c91e560b331232e6c00
Thompson Edolo <907685+verygreenboi@users.noreply.github.com>
2022-06-14 02:16:06 +0100

    Feature/controller (#35)
    * feat: add controller interface
    
    * fix: add respond method to controller type
    
    * fix: remove parse and respond from controller

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -3,13 +3,4 @@ import { Service } from "../service";
 
 export type Controller<T extends Service> = (service?: T) => {
   handle: (req: Request, res: Response) => Promise<void>;
-  parse?: <ParsedType>(
-    req: Request,
-    key: "body" | "params" | "query" | "headers"
-  ) => ParsedType;
-  respond: <PayloadType>(
-    res: Response,
-    status?: number,
-    payload?: PayloadType
-  ) => void;
 };
--- src/service/service.ts
+++ src/service/service.ts
@@ -1,1 +1,10 @@
-export interface Service {}
+export interface Service {
+  parse: <T>(req: Request, key: "body" | "params" | "query" | "headers") => T;
+  /**
+   * Respond to the request.
+   * @param {e.Response} res
+   * @param {number} status
+   * @param {T} payload
+   */
+  respond: <T>(res: Response, status?: number, payload?: T) => void;
+}
