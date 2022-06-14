c331f219b097576b70dfc51d0a13f1bfb8ee854c
Thompson Edolo <907685+verygreenboi@users.noreply.github.com>
2022-06-14 01:46:24 +0100

    feat: add controller interface (#33)

---
+++ src/controller/index.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { Service } from "../service";
+
+export type Controller<T extends Service> = (service?: T) => {
+  handle: (req: Request, res: Response) => Promise<void>;
+  parse?: <ParsedType>(
+    req: Request,
+    key: "body" | "params" | "query" | "headers"
+  ) => ParsedType;
+};
