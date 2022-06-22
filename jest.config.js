be2dc18a6b04529b0fd50f7b5538067deeab2089
Thompson Edolo <verygreenboi@live.com>
2022-06-22 22:50:03 +0100

    fix: improve pbac middleware coverage

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,11 @@
  */
 
 module.exports = {
+  preset: "ts-jest",
   clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   roots: ["<rootDir>/src"],
   testEnvironment: "node",
