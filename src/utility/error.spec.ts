088cc07159ba323890f824035c15381ae5bc3f19
Thompson Edolo <verygreenboi@live.com>
2022-06-22 23:16:05 +0100

    fix: improve api error class coverage

--- src/utility/error.spec.ts
+++ src/utility/error.spec.ts
@@ -1,8 +1,10 @@
 import { BaseError } from "./error";
 import { HttpStatusCode } from "./http-status-codes";
+import { APIError } from "./api-error";
 
 describe("Error handling", () => {
   let error: Error;
+  let apiError: APIError;
 
   describe("Base error", () => {
     beforeEach(() => {
@@ -20,6 +22,25 @@ describe("Error handling", () => {
 
     it("should have a message property", () => {
       expect(error.message).toEqual("message");
+    });
+  });
+
+  describe("Api error", () => {
+    beforeEach(() => {
+      apiError = new APIError(
+        "error",
+        "message",
+        HttpStatusCode.BAD_REQUEST,
+        false
+      );
+    });
+
+    it("should return an instance of BaseError", () => {
+      expect(apiError).toBeInstanceOf(BaseError);
+    });
+
+    it("should return an instance of APIError", () => {
+      expect(apiError).toBeInstanceOf(APIError);
     });
   });
 });
