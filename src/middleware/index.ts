2675a7cbbef22231096aeec58d0e6730cf7edd11
Thompson Edolo <907685+verygreenboi@users.noreply.github.com>
2022-06-22 22:33:46 +0100

    fix: expose pbac middleware (#53)

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,3 +1,4 @@
 export { hasTenantId } from "./has-tenant-id.middleware";
 export { permissions } from "./permissions.middleware";
+export { policyMiddleware } from "./pbac.middleware";
 export { setDbConnection } from "./establish-tenant-db-connection";
