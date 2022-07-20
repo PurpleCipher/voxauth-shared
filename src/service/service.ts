3a7a7dd5f23a01caabc83b25d2a5596382cf3598
Thompson Edolo <verygreenboi@live.com>
2022-07-20 17:27:23 +0100

    fix: make service methods optional

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,10 +1,10 @@
 export interface Service {
-  parse: <T>(req: Request, key: "body" | "params" | "query" | "headers") => T;
+  parse?: <T>(req: Request, key: "body" | "params" | "query" | "headers") => T;
   /**
    * Respond to the request.
    * @param {e.Response} res
    * @param {number} status
    * @param {T} payload
    */
-  respond: <T>(res: Response, status?: number, payload?: T) => void;
+  respond?: <T>(res: Response, status?: number, payload?: T) => void;
 }
