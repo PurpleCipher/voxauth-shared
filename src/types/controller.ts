4ecf68774b9b47d806dcc8dba7648226696f1f00
Thompson Edolo <verygreenboi@live.com>
2022-09-27 20:46:53 +0100

    feat(util): add init service controller wrapper

--- src/types/controller.ts
+++ src/types/controller.ts
@@ -1,10 +1,24 @@
 import { Request, Response } from "express";
-import { Service } from "../service";
+import { InitService, Service } from "../service";
+
+export type Handler = (req: Request, res: Response) => Promise<void>;
 
 export type Controller<T extends Service> = (service?: T) => {
-  handle: (req: Request, res: Response) => Promise<void>;
+  handle: Handler;
   parse?: <ParsedType>(
     req: Request,
     key: "body" | "params" | "query" | "headers"
   ) => ParsedType;
+};
+
+type IInitService = Service & InitService;
+
+export const initServiceControllerWrapper = async <T extends IInitService>(
+  service: T,
+  controller: Controller<T>,
+  isMultiTenant: boolean = false,
+  tenantId?: string
+): Promise<Handler> => {
+  await service.init(isMultiTenant, tenantId);
+  return controller(service).handle;
 };
