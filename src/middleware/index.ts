8e5365e098126725f5c8eec6edecabfd007483b5
Thompson Edolo <verygreenboi@live.com>
2022-10-23 22:29:58 +0100

    fix: update middleware exports

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,3 +2,4 @@ export { hasTenantId } from "./has-tenant-id.middleware";
 export { permissions } from "./permissions.middleware";
 export { policyMiddleware } from "./pbac.middleware";
 export { setDbConnection } from "./establish-tenant-db-connection";
+export { initServiceMiddleware } from "./init-service.middleware";
