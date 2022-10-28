f726d549cb6c02f13c49ee299dcd887fc53ca51a
Thompson Edolo <verygreenboi@live.com>
2022-10-28 20:55:32 +0100

    chore: add pagination implementation

--- src/service/db.service.ts
+++ src/service/db.service.ts
@@ -1,13 +1,19 @@
 import { DB } from "../utility";
-import { DBConnection, DBDocument, DBModel, DBSchema } from "../types";
+import {
+  DBConnection,
+  DBDocument,
+  DBModel,
+  DBPaginateModel,
+  DBSchema,
+} from "../types";
 import { InitService } from "./init.service";
 
 export interface DBService<D extends DBDocument> extends InitService {
   database: DB;
   connection?: DBConnection;
-  model?: DBModel<D>;
+  model?: DBModel<D> | DBPaginateModel<D>;
   getDbConnection: (tenantId?: string) => DBConnection;
-  getModel: (name: string, schema: DBSchema) => DBModel<D>;
+  getModel: (name: string, schema: DBSchema) => DBModel<D> | DBPaginateModel<D>;
   onConnect?: (resolve: () => void) => void;
   onDisconnect?: (resolve: () => void) => void;
   onError?: (reject: () => void) => void;
--- src/types/db.type.ts
+++ src/types/db.type.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
+import paginate from "mongoose-paginate-v2";
 
 export type DBConnection = mongoose.Connection;
 export type DBModel<T> = mongoose.Model<T>;
+export type DBPaginateModel<T> = mongoose.PaginateModel<T>;
 export type DBConnectOptions = mongoose.ConnectOptions;
 export type DBConnectionStates = mongoose.ConnectionStates;
+
+export const types = { paginate, mongoose };
