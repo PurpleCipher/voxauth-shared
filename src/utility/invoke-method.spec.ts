e818bccd949689295cf7cba185227852c08e5b18
Thompson Edolo <verygreenboi@live.com>
2022-11-06 18:18:11 +0100

    chore: fix prettier config

--- src/utility/invoke-method.spec.ts
+++ src/utility/invoke-method.spec.ts
@@ -1,112 +1,108 @@
-import axios from "axios";
-import { invokeServiceMethodFromDapr } from "./invoke-method";
+/** @format */
 
-jest.mock("axios");
+import axios from 'axios';
+import { invokeServiceMethodFromDapr } from './invoke-method';
 
-describe("Invoke method", () => {
-  let thenFn = jest.fn();
-  let catchFn = jest.fn();
+jest.mock('axios');
 
-  beforeEach(() => {
-    console.error = jest.fn();
-    console.log = jest.fn();
-  });
+describe('Invoke method', () => {
+	let thenFn = jest.fn();
+	let catchFn = jest.fn();
 
-  afterEach(() => {
-    jest.clearAllTimers();
-    jest.clearAllMocks();
-  });
+	beforeEach(() => {
+		console.error = jest.fn();
+		console.log = jest.fn();
+	});
 
-  describe("invoke payload", () => {
-    describe("no payload payload", () => {
-      beforeEach(async () => {
-        axios.post = jest.fn().mockRejectedValue(new Error());
-        await invokeServiceMethodFromDapr
-          .invokeMethod({
-            serviceName: "test",
-            port: 5000,
-            method: "test",
-          })
-          .then(thenFn)
-          .catch(catchFn);
-      });
-      it("should call dapr invoke api", () => {
-        expect(axios.post).toHaveBeenCalledWith(
-          "http://localhost:5000/v1.0/invoke/test/method/test",
-          {}
-        );
-      });
-    });
+	afterEach(() => {
+		jest.clearAllTimers();
+		jest.clearAllMocks();
+	});
 
-    describe("no payload provided", () => {
-      beforeEach(async () => {
-        axios.post = jest.fn().mockRejectedValue(new Error());
-        await invokeServiceMethodFromDapr
-          .invokeMethod({
-            serviceName: "test",
-            port: 5000,
-            method: "test",
-            payload: {
-              test: "test123",
-            },
-          })
-          .then(thenFn)
-          .catch(catchFn);
-      });
-      it("should call dapr invoke api", () => {
-        expect(axios.post).toHaveBeenCalledWith(
-          "http://localhost:5000/v1.0/invoke/test/method/test",
-          {
-            test: "test123",
-          }
-        );
-      });
-    });
-  });
+	describe('invoke payload', () => {
+		describe('no payload payload', () => {
+			beforeEach(async () => {
+				axios.post = jest.fn().mockRejectedValue(new Error());
+				await invokeServiceMethodFromDapr
+					.invokeMethod({
+						serviceName: 'test',
+						port: 5000,
+						method: 'test',
+					})
+					.then(thenFn)
+					.catch(catchFn);
+			});
+			it('should call dapr invoke api', () => {
+				expect(axios.post).toHaveBeenCalledWith('http://localhost:5000/v1.0/invoke/test/method/test', {});
+			});
+		});
 
-  describe("when invoke call fails", () => {
-    beforeEach(async () => {
-      axios.post = jest.fn().mockRejectedValue(new Error());
-      await invokeServiceMethodFromDapr
-        .invokeMethod({
-          serviceName: "test",
-          port: 5000,
-          method: "test",
-        })
-        .then(thenFn)
-        .catch(catchFn);
-    });
-    it("should not call the then fn", () => {
-      expect(thenFn).not.toBeCalled();
-    });
-    it("should throw error", () => {
-      expect(catchFn).toBeCalled();
-    });
-  });
-  describe("when invoke succeeds", () => {
-    beforeEach(async () => {
-      const res = {
-        data: {
-          test: "test",
-        },
-      };
-      axios.post = jest.fn().mockResolvedValue(res);
-      await invokeServiceMethodFromDapr
-        .invokeMethod({
-          serviceName: "test",
-          port: 5000,
-          method: "test",
-        })
-        .then(thenFn)
-        .catch(catchFn);
-    });
+		describe('no payload provided', () => {
+			beforeEach(async () => {
+				axios.post = jest.fn().mockRejectedValue(new Error());
+				await invokeServiceMethodFromDapr
+					.invokeMethod({
+						serviceName: 'test',
+						port: 5000,
+						method: 'test',
+						payload: {
+							test: 'test123',
+						},
+					})
+					.then(thenFn)
+					.catch(catchFn);
+			});
+			it('should call dapr invoke api', () => {
+				expect(axios.post).toHaveBeenCalledWith('http://localhost:5000/v1.0/invoke/test/method/test', {
+					test: 'test123',
+				});
+			});
+		});
+	});
 
-    it("should not call the then fn", () => {
-      expect(thenFn).toBeCalledWith({ test: "test" });
-    });
+	describe('when invoke call fails', () => {
+		beforeEach(async () => {
+			axios.post = jest.fn().mockRejectedValue(new Error());
+			await invokeServiceMethodFromDapr
+				.invokeMethod({
+					serviceName: 'test',
+					port: 5000,
+					method: 'test',
+				})
+				.then(thenFn)
+				.catch(catchFn);
+		});
+		it('should not call the then fn', () => {
+			expect(thenFn).not.toBeCalled();
+		});
+		it('should throw error', () => {
+			expect(catchFn).toBeCalled();
+		});
+	});
+	describe('when invoke succeeds', () => {
+		beforeEach(async () => {
+			const res = {
+				data: {
+					test: 'test',
+				},
+			};
+			axios.post = jest.fn().mockResolvedValue(res);
+			await invokeServiceMethodFromDapr
+				.invokeMethod({
+					serviceName: 'test',
+					port: 5000,
+					method: 'test',
+				})
+				.then(thenFn)
+				.catch(catchFn);
+		});
 
-    it("should throw error", () => {
-      expect(catchFn).not.toBeCalled();
-    });
-  });
+		it('should not call the then fn', () => {
+			expect(thenFn).toBeCalledWith({ test: 'test' });
+		});
+
+		it('should throw error', () => {
+			expect(catchFn).not.toBeCalled();
+		});
+	});
 });
--- src/utility/invoke-method.ts
+++ src/utility/invoke-method.ts
@@ -1,25 +1,27 @@
-import axios from "axios";
+/** @format */
+
+import axios from 'axios';
 
 export interface InvokeMethodConfig<T> {
-  serviceName: string;
-  port: number;
-  method: string;
-  payload?: T;
+	serviceName: string;
+	port: number;
+	method: string;
+	payload?: T;
 }
 
 const invokeMethod = async <T>(config: InvokeMethodConfig<T>) => {
-  try {
-    const { data } = await axios.post(
-      `http://localhost:${config.port}/v1.0/invoke/${config.serviceName}/method/${config.method}`,
-      config.payload ? config.payload : {}
-    );
-    return data;
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
+	try {
+		const { data } = await axios.post(
+			`http://localhost:${config.port}/v1.0/invoke/${config.serviceName}/method/${config.method}`,
+			config.payload ? config.payload : {},
+		);
+		return data;
+	} catch (e) {
+		console.error(e);
+		throw e;
+	}
 };
 
 export const invokeServiceMethodFromDapr = {
-  invokeMethod,
+	invokeMethod,
 };
